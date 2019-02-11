e3f2307f00c9bd092f9e3bc49c72591670b993be
Takaaki Moriyama <taka@trishan.org>
2019-02-11 23:05:03 +0900

    Add new project

---
+++ TopShelfSample/TopShelfSample/Beat.cs
@@ -0,0 +1,30 @@
+﻿using System.Timers;
+using NLog;
+
+namespace TopShelfSample
+{
+    public class Beat
+    {
+        private readonly Timer _timer;
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public Beat()
+        {
+            _timer = new Timer(1000)
+            {
+                AutoReset = true
+            };
+            _timer.Elapsed += (sender, eventArgs) => _logger.Info("I am fine");
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ TopShelfSample/TopShelfSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TopShelfSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<Beat>(s =>
+                {
+                    s.ConstructUsing(name => new Beat());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetDescription("Just beating program");
+                x.SetDisplayName("BeatingProgram");
+                x.SetServiceName("BeatingService");
+            });
+        }
+    }
+}
